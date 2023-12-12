dd883cdb4fc386ea46248a20a219ce386ca4a44e
ThomasHeap <thomaseheap@gmail.com>
2023-12-12 14:10:19 +0000

    test posterior sampling K using source term trick:

--- tests/test_reduce_Ks.py
+++ tests/test_reduce_Ks.py
@@ -2,7 +2,8 @@ import torch as t
 import functorch.dim
 from functorch.dim import Dim, dims
 
-from alan_simplified import Normal, Plate, BoundPlate, Group, Problem, IndependentSample
+
+from alan_simplified import Normal, Bernoulli, Plate, BoundPlate, Group, Problem, IndependentSample
 from alan_simplified.reduce_Ks import reduce_Ks, sample_Ks, logsumexp_sum
 
 import unittest
@@ -72,7 +73,65 @@ class TestSampleKs(unittest.TestCase):
 
         for k,v in samples.items():
             assert set(v.dims) == {self.Ndim, self.plate_1, self.plate_2}
-    
+            
+    def test_Sample_Ks_compare_Marginals(self):
+        P = Plate(
+            ab = Group(
+                a = Normal(0, 1),
+                b = Normal("a", 1),
+            ),
+            c = Normal(0, lambda a: a.exp()),
+            p1 = Plate(
+                d = Normal("a", 1),
+                p2 = Plate(
+                    e = Normal("d", 1.),
+                ),
+            ),
+        )
+
+        Q = Plate(
+            ab = Group(
+                a = Normal("a_mean", 1),
+                b = Normal("a", 1),
+            ),
+            c = Normal(0, lambda a: a.exp()),
+            p1 = Plate(
+                d = Normal("d_mean", 1),
+                p2 = Plate(
+                ),
+            ),
+        )
+        Q = BoundPlate(Q, params={'a_mean': t.zeros(()), 'd_mean':t.zeros(3, names=('p1',))})
+
+        all_platesizes = {'p1': 3, 'p2': 4}
+        data = {'e': t.randn(3, 4, names=('p1', 'p2'))}
+
+        prob = Problem(P, Q, all_platesizes, data)
+
+        sampling_type = IndependentSample
+        sample = prob.sample(3, True, sampling_type)
+
+        posterior_samples = list(sample.sample_posterior(num_samples=1000).values())
+
+        conditionals = sample.conditionals()
+
+        ab = posterior_samples[0].order(posterior_samples[0].dims) 
+        c = posterior_samples[1].order(posterior_samples[1].dims)
+        d = posterior_samples[2].order(posterior_samples[2].dims[1])
+
+        posterior_ab = t.bincount(ab) / 1000
+        source_term_ab = conditionals['ab'].order(conditionals['ab'].dims[0])
+        assert (t.abs(posterior_ab - source_term_ab) < 0.05).all()
+
+        posterior_c = t.bincount(c) / 1000
+        source_term_c = conditionals['c'].sum(conditionals['c'].dims[1]).order(conditionals['c'].dims[0])
+        assert (t.abs(posterior_c - source_term_c) < 0.05).all()
+
+        posterior_d = (t.bincount(d) / 1000).order(d.dims[0])
+        source_term_d = conditionals['d'].sum(conditionals['d'].dims[-1]).order(conditionals['d'].dims[1]).order(d.dims[0])
+        assert (t.abs(posterior_d - source_term_d) < 0.05).all()
+        
+        
 class Testlogsumexp_sum(unittest.TestCase):
 
     def setUp(self):
