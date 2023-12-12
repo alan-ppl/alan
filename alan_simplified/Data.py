ecb3169640889684186500529412f6d50343e5ea
ThomasHeap <thomaseheap@gmail.com>
2023-12-12 11:58:46 +0000

    removing unnecessary functions in Data()

--- alan_simplified/Data.py
+++ alan_simplified/Data.py
@@ -7,18 +7,4 @@ from .SamplingType import SamplingType
 
 
 class Data():
-    
-    def __init__(self):
-        pass
-
-    def sample(
-            self,
-            name:Optional[str],
-            scope: dict[str, Tensor], 
-            inputs_params: dict,
-            active_platedims:list[Dim],
-            all_platedims:dict[str, Dim],
-            groupvarname2Kdim:dict[str, Dim],
-            sampling_type:SamplingType,
-            reparam:bool):
-        pass
+    pass
