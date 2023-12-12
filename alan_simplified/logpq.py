91385e9f083e4a2528a9a67e22f95f26139d89c5
ThomasHeap <thomaseheap@gmail.com>
2023-12-12 15:09:46 +0000

    adding some lp_getter and collector functions to avoid repeated code in reduce Ks and the logpq methods

--- alan_simplified/logpq.py
+++ alan_simplified/logpq.py
@@ -33,75 +33,22 @@ def logPQ_plate(
     assert isinstance(data, dict)
     assert isinstance(extra_log_factors, dict)
 
-    #Push an extra plate, if not the top-layer plate (top-layer plate is signalled
-    #by name=None.
-    if name is not None:
-        active_platedims = [*active_platedims, all_platedims[name]]
-
-
-    #We want to pass back just the incoming scope, as nothing outside the plate can see
-    #variables inside the plate.  So `scope` is the internal scope, and `parent_scope`
-    #is the external scope we will pass back.
-    scope_P = update_scope_inputs_params(scope_P, inputs_params_P)
-    scope_Q = update_scope_inputs_params(scope_Q, inputs_params_Q)
-
-    assert set(P.prog.keys()) == set([*sample.keys(), *tree_values(data).keys()])
-
-    lps = list(tree_values(extra_log_factors).values())
-
-    #Dump all the scope for Q directly
-    #That allows P and Q to have different orders.
-    #Note that we already know Q has a valid order, because we sampled from Q
-    for childname, childQ in Q.prog.items():
-        childsample = sample[childname]
-        scope_Q = update_scope_sample(scope_Q, childname, childQ, childsample)
-
-    for childname, childP in P.prog.items():
-        childQ = Q.prog.get(childname) 
-
-        #childQ doesn't necessarily have a distribution if sample_data is data.
-        #childQ defaults to None in that case.
-
-        if isinstance(childP, Dist):
-            assert isinstance(childQ, (Dist, type(None), Data))
-            method = logPQ_dist
-        elif isinstance(childP, Plate):
-            assert isinstance(childQ, Plate)
-            method = logPQ_plate
-        else:
-            isinstance(childP, Group)
-            assert isinstance(childQ, Group)
-            method = logPQ_group
-
-        lp = method(
-            name=childname,
-            P=childP, 
-            Q=childQ, 
-            sample=sample.get(childname),
-            data=data.get(childname),
-            inputs_params_P=inputs_params_P.get(childname),
-            inputs_params_Q=inputs_params_Q.get(childname),
-            extra_log_factors=extra_log_factors.get(childname),
-            scope_P=scope_P, 
-            scope_Q=scope_Q, 
-            active_platedims=active_platedims,
-            all_platedims=all_platedims,
-            groupvarname2Kdim=groupvarname2Kdim,
-            sampling_type=sampling_type,
-            split=split)
-        lps.append(lp)
-
-        childsample = sample.get(childname)
-        if childsample is not None:
-            scope_P = update_scope_sample(scope_P, childname, childP, childsample)
-
-    #Collect all Ks in the plate
-    all_Ks = []
-    for varname, dist in Q.prog.items():
-        if isinstance(dist, (Dist, Group)):
-            all_Ks.append(groupvarname2Kdim[varname])
-        else:
-            assert isinstance(dist, (Plate, Data))
+    lps, all_Ks, scope_P, scope_Q, active_platedims = lp_getter(
+        name=name,
+        P=P, 
+        Q=Q, 
+        sample=sample, 
+        inputs_params_P=inputs_params_P,
+        inputs_params_Q=inputs_params_Q,
+        data=data,
+        extra_log_factors=extra_log_factors, 
+        scope_P=scope_P, 
+        scope_Q=scope_Q, 
+        active_platedims=active_platedims,
+        all_platedims=all_platedims,
+        groupvarname2Kdim=groupvarname2Kdim,
+        sampling_type=sampling_type,
+        split=split)
 
     #Sum out Ks
     lp = reduce_Ks(lps, all_Ks)
@@ -203,3 +150,95 @@ def logPQ_group(
 
     logPQ = total_logP - total_logQ - math.log(Kdim.size)
     return logPQ
+
+
+def lp_getter(
+        name:Optional[str],
+        P:Plate, 
+        Q:Plate, 
+        sample: dict, 
+        inputs_params_P: dict,
+        inputs_params_Q: dict,
+        data: dict,
+        extra_log_factors: dict, 
+        scope_P: dict[str, Tensor], 
+        scope_Q: dict[str, Tensor], 
+        active_platedims:list[Dim],
+        all_platedims:dict[str: Dim],
+        groupvarname2Kdim:dict[str, Tensor],
+        sampling_type:SamplingType,
+        split:Optional[Split]):
+    """Traverses Q according to the structure of P collecting log probabilities
+    
+    """
+        #Push an extra plate, if not the top-layer plate (top-layer plate is signalled
+    #by name=None.
+    if name is not None:
+        active_platedims = [*active_platedims, all_platedims[name]]
+
+
+    #We want to pass back just the incoming scope, as nothing outside the plate can see
+    #variables inside the plate.  So `scope` is the internal scope, and `parent_scope`
+    #is the external scope we will pass back.
+    scope_P = update_scope_inputs_params(scope_P, inputs_params_P)
+    scope_Q = update_scope_inputs_params(scope_Q, inputs_params_Q)
+
+    assert set(P.prog.keys()) == set([*sample.keys(), *tree_values(data).keys()])
+
+    lps = list(tree_values(extra_log_factors).values())
+
+    #Dump all the scope for Q directly
+    #That allows P and Q to have different orders.
+    #Note that we already know Q has a valid order, because we sampled from Q
+    for childname, childQ in Q.prog.items():
+        childsample = sample[childname]
+        scope_Q = update_scope_sample(scope_Q, childname, childQ, childsample)
+
+    for childname, childP in P.prog.items():
+        childQ = Q.prog.get(childname) 
+
+        #childQ doesn't necessarily have a distribution if sample_data is data.
+        #childQ defaults to None in that case.
+
+        if isinstance(childP, Dist):
+            assert isinstance(childQ, (Dist, type(None), Data))
+            method = logPQ_dist
+        elif isinstance(childP, Plate):
+            assert isinstance(childQ, Plate)
+            method = logPQ_plate
+        else:
+            isinstance(childP, Group)
+            assert isinstance(childQ, Group)
+            method = logPQ_group
+
+        lp = method(
+            name=childname,
+            P=childP, 
+            Q=childQ, 
+            sample=sample.get(childname),
+            data=data.get(childname),
+            inputs_params_P=inputs_params_P.get(childname),
+            inputs_params_Q=inputs_params_Q.get(childname),
+            extra_log_factors=extra_log_factors.get(childname),
+            scope_P=scope_P, 
+            scope_Q=scope_Q, 
+            active_platedims=active_platedims,
+            all_platedims=all_platedims,
+            groupvarname2Kdim=groupvarname2Kdim,
+            sampling_type=sampling_type,
+            split=split)
+        lps.append(lp)
+
+        childsample = sample.get(childname)
+        if childsample is not None:
+            scope_P = update_scope_sample(scope_P, childname, childP, childsample)
+
+    #Collect all Ks in the plate
+    all_Ks = []
+    for varname, dist in Q.prog.items():
+        if isinstance(dist, (Dist, Group)):
+            all_Ks.append(groupvarname2Kdim[varname])
+        else:
+            assert isinstance(dist, (Plate, Data))
+            
+    return lps, all_Ks, scope_P, scope_Q, active_platedims
