59bd3c98868ee4eafa5989e05a3b9fd7fbacdcb5
Laurence Aitchison <laurence.aitchison@gmail.com>
2023-12-20 10:56:53 +0000

    Draft function to uniformise arguments to moments

--- alan_simplified/moments.py
+++ alan_simplified/moments.py
@@ -42,12 +42,12 @@ class CompoundMoment(Moment):
 
 
 
-class Mean(RawMoment)
+class Mean(RawMoment):
     @staticmethod
     def f(x):
         return x
 
-class Mean2(RawMoment)
+class Mean2(RawMoment):
     @staticmethod
     def f(x):
         return x*x
@@ -57,3 +57,46 @@ class Var(CompoundMoment):
     @staticmethod
     def combiner(mean, mean2):
         return mean2 - mean**2
+
+def uniformise_moment_args(*args):
+    """
+    moment can be called in a bunch of different ways.  For a single variable/set of variables:
+    * `sample.moments("a", Mean)`
+    * `sample.moments("a", (Mean, Var))`
+    * `sample.moments(("a", "b"), Cov)`
+
+    For multiple variables:
+    sample.moments({
+        "a": Mean,
+        "b": (Mean, Var),
+        ("a", "b"): Cov
+    })
+
+    This function converts all these argument formats into a uniform dictionary, mapping tuples of input variables to tuples of moments.
+    """
+    mom_args_exception = Exception(".moment must be called as ...")
+
+    #Converts everthing to a dict.
+    if   1 == len(args):
+        if not isinstance(args, dict):
+            raise mom_args_exception
+    elif 2 == len(args):
+        args = {args[0]: args[1]}
+    else:
+        raise mom_args_exception
+
+    uniform_arg_dict = {}
+    for k, v in args.items():
+        if not isinstance(k, (tuple, str)):
+            raise mom_args_exception
+        if not isinstance(v, (tuple, Moment)):
+            raise mom_args_exception
+
+        if not isinstance(k, tuple):
+            k = (k,)
+        if not isinstance(v, tuple):
+            v = (v,)
+
+        uniform_arg_dict[k] = v
+
+    return uniform_arg_dict
