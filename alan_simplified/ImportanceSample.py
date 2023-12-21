0314af36f5f586d3f79a8f80520f75d4bfea8daa
Laurence Aitchison <laurence.aitchison@gmail.com>
2023-12-21 10:23:03 +0000

    Skeleton for ExtendedImportanceSample

--- alan_simplified/ImportanceSample.py
+++ alan_simplified/ImportanceSample.py
@@ -2,16 +2,7 @@ from .utils import *
 from .Plate import flatten_tree
 from .moments import uniformise_moment_args, postproc_moment_outputs
 
-class ImportanceSample:
-    def __init__(self, problem, samples_tree, Ndim):
-        """
-        samples is tree-structured torchdim (as we might need to use it for extended).
-        """
-        self.problem = problem
-        self.samples_tree = samples_tree
-        self.samples_flatdict = flatten_tree(samples_tree)
-        self.Ndim = Ndim
-
+class AbstractImportanceSample():
     def dump(self):
         """
         User-facing method that returns a flat dict of named tensors, with N as the first dimension.
@@ -31,3 +22,40 @@ class ImportanceSample:
             result[varnames] = tuple(moments)
 
         return postproc_moment_outputs(result, raw_moms)
+
+class ImportanceSample(AbstractImportanceSample):
+    def __init__(self, problem, samples_tree, Ndim):
+        """
+        samples is tree-structured torchdim (as we might need to use it for extended).
+        """
+        self.problem = problem
+        self.samples_tree = samples_tree
+        self.samples_flatdict = flatten_tree(samples_tree)
+        self.Ndim = Ndim
+
+    def extend(self, extend_platesizes:dict[str, int]):
+        """
+        User-facing method that extends sample by drawing the rest from the prior.
+
+        Note: doesn't take any extended data!!!
+
+        returns ExtendedImportanceSample
+        """
+        #Returns ExtendedImportanceSample
+        pass
+
+
+class ExtendedImportanceSample:
+    def __init__(self, problem, samples_tree, Ndim, extended_platedims):
+        """
+        samples is tree-structured torchdim (as we might need to use it for extended).
+        """
+        self.problem = problem
+        self.samples_tree = samples_tree
+        self.samples_flatdict = flatten_tree(samples_tree)
+        self.Ndim = Ndim
+        self.extended_platedims = extended_platedims
+
+    def predictive_ll(self, data:dict[str, Tensor]):
+        pass
+
