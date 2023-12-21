f1b0855826f0835b5592d795751b5ab4ce1d8df2
sambowyer <sambowyer2@gmail.com>
2023-12-21 14:51:47 +0000

    ImportanceSample and ExtendedImportanceSample for predictive stuff

--- alan_simplified/Group.py
+++ alan_simplified/Group.py
@@ -61,17 +61,12 @@ class Group():
             inputs_params:dict,
             original_platedims:dict[str, Dim],
             extended_platedims:dict[str, Dim],
-            active_original_platedims:list[Dim],
             active_extended_platedims:list[Dim],
             Ndim:Dim,
             reparam:bool,
-            original_data:Optional[dict[str, Tensor]],
-            extended_data:Optional[dict[str, Tensor]]):
-
+            original_data:dict):
+        
         result = {}       #This is the sample returned.
-
-        original_ll = {}
-        extended_ll = {}
 
         #resampled scope is the scope used in here when sampling from the Group
         scope = self.filter_scope(scope)
@@ -80,28 +75,62 @@ class Group():
         # logprobs of original and extended data IF extended_data is provided, i.e. not None)
         for name, dist in self.prog.items():
 
-            childsample, child_original_ll, child_extended_ll = dist.sample_extended(
+            childsample = dist.sample_extended(
                 sample=sample.get(name),
                 name=name,
                 scope=scope,
                 inputs_params=inputs_params,
                 original_platedims=original_platedims,
                 extended_platedims=extended_platedims,
-                active_original_platedims=active_original_platedims,
                 active_extended_platedims=active_extended_platedims,
                 Ndim=Ndim,
                 reparam=reparam,
                 original_data=original_data,
-                extended_data=extended_data
             )
 
             scope[name]  = childsample
             result[name] = childsample
 
-            original_ll = {**original_ll, **child_original_ll}
-            extended_ll = {**extended_ll, **child_extended_ll}
+        return result
+    
+    def predictive_ll(
+        self,
+        sample:dict,
+        name:str,
+        scope:dict[str, Tensor],
+        inputs_params:dict,
+        original_platedims:dict[str, Dim],
+        extended_platedims:dict[str, Dim],
+        original_data: dict[str, Tensor],
+        extended_data: dict[str, Tensor]):
+        
+        #resampled scope is the scope used in here when sampling from the Group
+        scope = self.filter_scope(scope)
 
-        return result, original_ll, extended_ll
+        original_lls, extended_lls = {}, {}
+
+        # Loop through all dists in the group and sample from them (plus potentially get 
+        # logprobs of original and extended data IF extended_data is provided, i.e. not None)
+        for name, dist in self.prog.items():
+
+            child_original_lls, child_extended_lls = dist.predictive_ll(
+                sample=sample.get(name),
+                name=name,
+                scope=scope,
+                inputs_params=inputs_params,
+                original_platedims=original_platedims,
+                extended_platedims=extended_platedims,
+                original_data=original_data,
+                extended_data=extended_data
+            )
+
+            scope[name]  = sample.get(name)
+
+            original_lls = {**original_lls, **child_original_lls}
+            extended_lls = {**extended_lls, **child_extended_lls}
+
+        return original_lls, extended_lls
+
 
     def all_prog_names(self):
         return self.prog.keys()
--- alan_simplified/ImportanceSample.py
+++ alan_simplified/ImportanceSample.py
@@ -1,5 +1,6 @@
+from typing import Optional
 from .utils import *
-from .Plate import flatten_tree
+from .Plate import flatten_tree, tensordict2tree
 from .moments import uniformise_moment_args, postproc_moment_outputs
 
 class AbstractImportanceSample():
@@ -33,7 +34,7 @@ class ImportanceSample(AbstractImportanceSample):
         self.samples_flatdict = flatten_tree(samples_tree)
         self.Ndim = Ndim
 
-    def extend(self, extend_platesizes:dict[str, int]):
+    def extend(self, extended_platesizes:dict[str, int], reparam:bool, extended_inputs: Optional[dict[str, Tensor]]):
         """
         User-facing method that extends sample by drawing the rest from the prior.
 
@@ -41,12 +42,45 @@ class ImportanceSample(AbstractImportanceSample):
 
         returns ExtendedImportanceSample
         """
-        #Returns ExtendedImportanceSample
-        pass
 
+        assert isinstance(extended_platesizes, dict)
+        assert isinstance(reparam, bool)
+        assert isinstance(extended_inputs, dict) or extended_inputs is None
+        if extended_inputs is None:
+            extended_inputs = {}
+        
+        # If all_platesizes is missing some plates from self.problem.all_platedims,
+        # add them in without changing their sizes.
+        for name, dim in self.problem.all_platedims.items():
+            if name not in extended_platesizes:
+                extended_platesizes[name] = dim.size
 
-class ExtendedImportanceSample:
-    def __init__(self, problem, samples_tree, Ndim, extended_platedims):
+        # Check that extend_platesizes contains no extra plates.
+        assert set(extended_platesizes.keys()) == set(self.problem.all_platedims.keys())
+
+        # Create the new platedims from the platesizes.
+        extended_platedims = {name: Dim(name, size) for name, size in extended_platesizes.items()}
+
+        # Will need to add the extended inputs to the scope
+        all_inputs_params = tensordict2tree(self.problem.P.plate, named2dim_dict(extended_inputs, extended_platedims))
+
+        extended_sample = self.problem.P.plate.sample_extended(
+            sample=self.samples_tree,
+            name=None,
+            scope={},
+            inputs_params=all_inputs_params,
+            original_platedims=self.problem.all_platedims,
+            extended_platedims=extended_platedims,
+            active_extended_platedims=[],
+            Ndim=self.Ndim,
+            reparam=reparam,
+            original_data=self.problem.data,
+        )
+
+        return ExtendedImportanceSample(self.problem, extended_sample, self.Ndim, extended_platedims, extended_inputs)
+
+class ExtendedImportanceSample(AbstractImportanceSample):
+    def __init__(self, problem, samples_tree, Ndim, extended_platedims, extended_inputs):
         """
         samples is tree-structured torchdim (as we might need to use it for extended).
         """
@@ -55,7 +89,61 @@ class ExtendedImportanceSample:
         self.samples_flatdict = flatten_tree(samples_tree)
         self.Ndim = Ndim
         self.extended_platedims = extended_platedims
+        self.extended_inputs = extended_inputs
 
     def predictive_ll(self, data:dict[str, Tensor]):
-        pass
+        '''
+        User-facing method that computes the predictive log-likelihood of the extended data.
+        '''
+        assert isinstance(data, dict)
+
+        # Convert data to torchdim
+        extended_data = named2dim_tensordict(self.extended_platedims, data)
+        
+        original_data = flatten_tree(self.problem.data)
+
+        # If data is missing (i.e. not being extended), add it in from the original(non-extended) data.
+        for name, data_tensor in original_data.items():
+            if name not in data.keys():
+                data[name] = data_tensor
+
+        # Check that data contains no extra data names.
+        assert set(data.keys()) == set(original_data.keys())
+
+        # Will need to add the extended inputs to the scope
+        all_inputs_params = tensordict2tree(self.problem.P.plate, named2dim_dict(self.extended_inputs, self.extended_platedims))
+
+        lls_train, lls_all = self.problem.P.plate.predictive_ll(
+            sample=self.samples_tree,
+            name=None,
+            scope={},
+            inputs_params=all_inputs_params,
+            original_platedims=self.problem.all_platedims,
+            extended_platedims=self.extended_platedims,
+            original_data=original_data,
+            extended_data=extended_data,
+        )
+
+        # If we have lls for a variable in the training data, we should also have lls
+        # for it in the all (training+test) data.
+        assert set(lls_all.keys()) == set(lls_train.keys())
+
+        result = {}
+        for varname in lls_all:
+            ll_all   = lls_all[varname]
+            ll_train = lls_train[varname]
+
+            dims_all   = [dim for dim in ll_all.dims   if dim is not self.Ndim]
+            dims_train = [dim for dim in ll_train.dims if dim is not self.Ndim]
+            assert len(dims_all) == len(dims_train)
+
+            if 0 < len(dims_all):
+                # Sum over plates
+                ll_all   = ll_all.sum(dims_all)
+                ll_train = ll_train.sum(dims_train)
+
+            # Take mean over Ndim
+            result[varname] = logmeanexp_dims(ll_all - ll_train, (self.Ndim,))
+
+        return result
 
