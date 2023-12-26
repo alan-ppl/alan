22b6ebbbbbd15cb59795b87bdbd94e380c0ce063
Laurence Aitchison <laurence.aitchison@gmail.com>
2023-12-26 10:48:08 +0000

    Forgotten tests

---
+++ tests/linear_gaussian_latents_batch.py
@@ -0,0 +1,58 @@
+import torch as t
+from alan_simplified import Bernoulli, Beta, Plate, BoundPlate, Group, Problem, Data, mean, mean2, Normal, PermutationSampler, CategoricalSampler
+from TestProblem import TestProblem
+
+prior_mean = t.randn(2)
+prior_scale = t.tensor([1., 2.])
+prior_var = prior_scale**2
+prior_prec = 1/prior_var
+
+z_scale = t.tensor([1.3, 1.6])
+d_scale = t.tensor([2., 3.])
+
+like_var = z_scale**2 + d_scale**2
+like_prec = 1/like_var
+
+N = 10
+data = 1.5+t.randn(N, 2).refine_names('T', None)
+post_prec = prior_prec + data.shape[0]*like_prec
+post_mean = (prior_prec*prior_mean + like_prec*data.sum('T')) / post_prec
+
+P = Plate(
+    a = Normal(prior_mean, prior_scale),
+    T = Plate(
+        z = Normal('a', z_scale),
+        d = Normal('z', d_scale),
+    ),
+)
+
+Q = Plate(
+    a = Normal(t.zeros(2), 4),
+    T = Plate(
+        z = Normal(lambda a: 0.5*a, 6),
+        d = Data(),
+    ),
+)
+
+P = BoundPlate(P)
+Q = BoundPlate(Q)
+
+all_platesizes = {'T': N}
+data = {'d': data}
+problem = Problem(P, Q, all_platesizes, data)
+
+moments = [('a', mean), ('a', mean2), ('z', mean), ('z', mean2)]
+known_moments = {
+    ('a', mean): post_mean,
+    ('a', mean2): post_mean**2 + 1/post_prec,
+}
+
+
+
+tp = TestProblem(
+    problem, 
+    moments, 
+    known_moments=known_moments, 
+    moment_K=1000, 
+)
+
---
+++ tests/linear_multivariate_gaussian_param.py
@@ -0,0 +1,52 @@
+import torch as t
+from alan_simplified import Bernoulli, Beta, Plate, BoundPlate, Group, Problem, Data, mean, mean2, MultivariateNormal
+from TestProblem import TestProblem
+
+F = 2
+prior_mean = t.randn(F)
+A = t.randn(F, F)
+prior_cov = A @ A.mT
+prior_prec = t.inverse(prior_cov)
+
+ap_mean = t.randn(F)
+B = t.randn(F, F)
+ap_cov = B@B.mT + 2*t.eye(F)
+
+C = t.randn(F, F)
+like_cov = C @ C.mT
+like_prec = t.inverse(like_cov)
+
+N = 10
+data = 1.5+t.randn(N, F)
+post_prec = prior_prec + data.shape[0]*like_prec
+post_cov = t.inverse(post_prec)
+post_mean = post_cov @ (prior_prec@prior_mean + like_prec@data.sum(0))
+
+
+P = Plate(
+    a = MultivariateNormal(prior_mean, prior_cov),
+    T = Plate(
+        d = MultivariateNormal('a', like_cov),
+    ),
+)
+
+Q = Plate(
+    a = MultivariateNormal(ap_mean, ap_cov),
+    T = Plate(
+        d = Data(),
+    ),
+)
+
+P = BoundPlate(P)
+Q = BoundPlate(Q)
+
+all_platesizes = {'T': N}
+data = {'d': data.refine_names('T', None)}
+problem = Problem(P, Q, all_platesizes, data)
+
+moments = [('a', mean)]
+known_moments = {
+    ('a', mean): post_mean,
+}
+
+tp = TestProblem(problem, moments, moment_K=10000)
