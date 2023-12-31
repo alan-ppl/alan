0e94f2429039187aa0afc84c3da858cba3aeb8e1
Laurence Aitchison <laurence.aitchison@gmail.com>
2023-12-31 14:05:37 +0000

    Python version

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ setup(
     license = "MIT Licence",
     packages=['alan'],
     package_dir={'':'src'},
+    python_requires='>=3.9.0',
     install_requires=[
         "torch>=2.0.0",
         "opt_einsum",
