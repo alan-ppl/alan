448660eaf166172a430d1bd65696b4fdf292e79f
Laurence Aitchison <laurence.aitchison@gmail.com>
2024-01-08 12:53:56 +0000

    Fixed Thomas's issue with plated OptParam

--- src/alan/Stores.py
+++ src/alan/Stores.py
@@ -1,48 +1,49 @@
 import torch
 
-class BufferStore(torch.nn.Module):
-    """
-    Holds buffers in a dictionary
-    makes sure they e.g. get moved to device correctly.
-    """
+def filter_not_none(xs):
+    return [x for x in xs if (x is not None)]
 
+class TensorStore(torch.nn.Module):
     def __init__(self, bufferdict:dict):
         super().__init__()
+        self.keys = []
+        self.names = {}
+
         restricted_keys = dir(self)
 
-        self.keys = []
         for k, v in bufferdict.items():
             assert k not in restricted_keys
-            assert isinstance(v, torch.Tensor)
             self.keys.append(k)
 
-            self.register_buffer(k, v)
+            assert isinstance(v, torch.Tensor)
+            names = filter_not_none(v.names)
+            v = v.align_to(*names, ...).rename(None)
+            self.register_tensor(k, v)
+
+            self.names[k] = names
 
     def to_dict(self):
-        return {k: getattr(self, k) for k in self.keys}
+        return {k: getattr(self, k).refine_names(*self.names[k], ...) for k in self.keys}
 
-class ParameterStore(torch.nn.Module):
+class BufferStore(TensorStore):
     """
     Holds buffers in a dictionary
     makes sure they e.g. get moved to device correctly.
     """
+    def register_tensor(self, k, v):
+        self.register_buffer(k, v)
 
-    def __init__(self, paramdict:dict):
-        super().__init__()
-        restricted_keys = dir(self)
 
-        self.keys = []
-        for k, v in paramdict.items():
-            assert k not in restricted_keys
-            assert isinstance(v, torch.Tensor)
-            self.keys.append(k)
+class ParameterStore(TensorStore):
+    """
+    Holds buffers in a dictionary
+    makes sure they e.g. get moved to device correctly.
+    """
+    def register_tensor(self, k, v):
+        if not isinstance(v, torch.nn.Parameter):
+            v = torch.nn.Parameter(v)
+        self.register_parameter(k, v)
 
-            if not isinstance(v, torch.nn.Parameter):
-                v = torch.nn.Parameter(v)
-            self.register_parameter(k, v)
-
-    def to_dict(self):
-        return {k: getattr(self, k) for k in self.keys}
 
 class ModuleStore(torch.nn.Module):
     def __init__(self, moddict:dict):
