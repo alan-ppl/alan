3a28055d0a68284b593ae36830cff92089756f5d
ThomasHeap <thomaseheap@gmail.com>
2024-01-30 12:02:18 +0000

    adding scripts

---
+++ examples/run_radon.sh
@@ -0,0 +1,5 @@
+lbatch -c 1 -g 1 --gputype A100 --exclude_40G_A100 -t 1 -m 64 -a cosc020762 -n MVI --queue cnu --cmd 'python runner.py model=radon method=vi'
+
+lbatch -c 1 -g 1 --gputype A100 --exclude_40G_A100 -t 1 -m 64 -a cosc020762 -n MRWS --queue cnu --cmd 'python runner.py model=radon method=rws'
+
+lbatch -c 1 -g 1 --gputype A100 --exclude_40G_A100 -t 1 -m 64 -a cosc020762 -n MQEM --queue cnu --cmd 'python runner.py model=radon method=qem'
