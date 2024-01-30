4942c02e9617b9b1d7787440e06a335c74f67d06
ThomasHeap <thomaseheap@gmail.com>
2024-01-30 14:06:04 +0000

    small change to reparam models

--- examples/movielens_reparam/movielens_reparam.py
+++ examples/movielens_reparam/movielens_reparam.py
@@ -32,7 +32,7 @@ def load_data_covariates(device, run=0, data_dir='data/', fake_data=False):
 
 def get_P(platesizes, covariates):
     P = Plate(
-        mu_z = Normal(t.zeros((d_z,)), t.ones((d_z,)) / math.log(1000)),
+        mu_z = Normal(t.zeros((d_z,)), t.ones((d_z,)) / 1000),
         psi_z = Normal(t.zeros((d_z,)), t.ones((d_z,))),
 
         plate_1 = Plate(
