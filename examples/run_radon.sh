60ff86061d78c59c0d7b85582a343ae868f43000
thomas <cp20141@bristol.ac.uk>
2024-01-30 12:03:18 +0000

    Update run_radon.sh

--- examples/run_radon.sh
+++ examples/run_radon.sh
@@ -1,5 +1,5 @@
-lbatch -c 1 -g 1 --gputype A100 --exclude_40G_A100 -t 1 -m 64 -a cosc020762 -n MVI --queue cnu --cmd 'python runner.py model=radon method=vi'
+lbatch -c 1 -g 1 --gputype A100 --exclude_40G_A100 -t 1 -m 64 -a !!!!!!!!!! -n MVI --queue cnu --cmd 'python runner.py model=radon method=vi'
 
-lbatch -c 1 -g 1 --gputype A100 --exclude_40G_A100 -t 1 -m 64 -a cosc020762 -n MRWS --queue cnu --cmd 'python runner.py model=radon method=rws'
+lbatch -c 1 -g 1 --gputype A100 --exclude_40G_A100 -t 1 -m 64 -a !!!!!!!!!! -n MRWS --queue cnu --cmd 'python runner.py model=radon method=rws'
 
-lbatch -c 1 -g 1 --gputype A100 --exclude_40G_A100 -t 1 -m 64 -a cosc020762 -n MQEM --queue cnu --cmd 'python runner.py model=radon method=qem'
+lbatch -c 1 -g 1 --gputype A100 --exclude_40G_A100 -t 1 -m 64 -a !!!!!!!!!! -n MQEM --queue cnu --cmd 'python runner.py model=radon method=qem'
