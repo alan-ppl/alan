d46f645cc41c0427a0c0d439568c7b7c8a0171c9
sambowyer <sambowyer2@gmail.com>
2024-02-06 11:35:43 +0000

    Add option to ignore certain lrs in plot

--- examples/moments_paper_plots/moments_paper_plots.py
+++ examples/moments_paper_plots/moments_paper_plots.py
@@ -5,7 +5,14 @@ import torch as t
 
 ALL_MODEL_NAMES = ['bus_breakdown', 'chimpanzees', 'movielens', 'occupancy']
 
-DEFAULT_ALPHA_FUNC = lambda i, num_lrs: 1 if i == 0 else 1 - 0.5*i/(num_lrs-1)
+DEFAULT_ALPHA_FUNC = lambda i, num_lrs: 1 if i >= 0 else 1 - 0.5*i/(num_lrs-1)
+
+DEFAULT_MODEL_METHOD_LRS_TO_IGNORE = {'bus_breakdown': {'vi': [0.1, 0.03, 0.01], 'rws': [0.3, 0.1, 0.03]},
+                                      'chimpanzees': {'vi': [0.1, 0.03, 0.01], 'rws': [0.3, 0.1, 0.03]},
+                                      'movielens': {'vi': [0.3, 0.03, 0.01], 'rws': [0.3, 0.1, 0.03]},
+                                      'occupancy': {'vi': [0.03, 0.01], 'rws': [0.3, 0.1, 0.03]}}
+
+DEFAULT_MODEL_METHOD_LRS_TO_IGNORE = {model_name: {} for model_name in ALL_MODEL_NAMES}
 
 def load_results(model_name, method_name, fake_data, dataset_seed=0):
     # if method_name in ['mpis', 'global_is']:
@@ -117,9 +124,12 @@ def plots_IS_per_K_all_models(save_pdf=False, scatter=False, x_axis_time=False):
                 #     else:
                 #         ax[k, i].plot(all_results[model_name][method]['Ks'], all_results[model_name][method][key].cpu(), label=method.upper(), color=colour)
                 #     ax[k, i].errorbar(all_results[model_name][method]['Ks'], all_results[model_name][method][key].cpu(), yerr=0, label=method.upper(), color=colour)
-                        
-                ax[k, i].set_xlabel('K')
-                ax[k, i].tick_params(axis='x', rotation=45)
+                
+                if x_axis_time:
+                    ax[k, i].set_xlabel('Time (s)')
+                else:
+                    ax[k, i].set_xlabel('K')
+                    ax[k, i].tick_params(axis='x', rotation=45)
 
         ax[0, i].set_title(model_name.upper())
 
@@ -137,13 +147,17 @@ def plots_IS_per_K_all_models(save_pdf=False, scatter=False, x_axis_time=False):
         plt.savefig(f"plots/all_IS_per_K{'_TIME' if x_axis_time else ''}.pdf")
 
 
-def plot_iterative_vs_IS_all_K_one_method(model_name, iterative_methods = ['vi', 'rws', 'HMC'], save_pdf=False, x_lim_iters=10, log_x=False):
+def plot_iterative_vs_IS_all_K_one_model(model_name, iterative_methods = ['vi', 'rws', 'HMC'], save_pdf=False, x_lim_iters=10, log_x=False, model_method_lrs_to_ignore=DEFAULT_MODEL_METHOD_LRS_TO_IGNORE):
     all_results = {}
     for method in ['mpis', 'global_is'] + iterative_methods:
         all_results[method] = load_results(model_name, method, False)
+        if model_name == 'chimpanzees' and method in iterative_methods:
+            breakpoint()
         all_results[method]['MSEs'] = sum_all_MSEs(all_results[method])
 
         fake_results = load_results(model_name, method, True)
+        if model_name == 'chimpanzees' and method in iterative_methods:
+            breakpoint()
         all_results[method]['MSEs_fake'] = sum_all_MSEs(fake_results)
         all_results[method]['times']['moments_fake'] = fake_results['times']['moments']
 
@@ -162,6 +176,8 @@ def plot_iterative_vs_IS_all_K_one_method(model_name, iterative_methods = ['vi',
         colour = 'C' + str(i + 2)
 
         for lr_idx, lr in enumerate(all_results[method]['lrs']):
+            if lr in model_method_lrs_to_ignore[model_name].get(method, []):
+                continue
             alpha = DEFAULT_ALPHA_FUNC(lr_idx, len(all_results[method]['lrs']))
 
             ax[0].plot(all_results[method]['times']['elbos'][lr_idx].mean(-1).cumsum(0)[:x_lim_iters], all_results[method]['elbos'][lr_idx].mean(-1)[:x_lim_iters], label=f"{method.upper()} lr={lr}", color=colour, alpha=alpha)
@@ -190,7 +206,7 @@ def plot_iterative_vs_IS_all_K_one_method(model_name, iterative_methods = ['vi',
     if save_pdf:
         plt.savefig(f"plots/{model_name}_iterative_vs_IS_all_K{'_log_x' if log_x else ''}.pdf")
 
-def plot_iterative_vs_IS_single_K_one_model(model_name, iterative_methods = ['vi', 'rws', 'HMC'], mpis_K = 10, global_is_K = 1000, save_pdf=False, x_lim_iters=10, log_x=False):
+def plot_iterative_vs_IS_single_K_one_model(model_name, iterative_methods = ['vi', 'rws', 'HMC'], mpis_K = 10, global_is_K = 1000, save_pdf=False, x_lim_iters=10, log_x=False, model_method_lrs_to_ignore=DEFAULT_MODEL_METHOD_LRS_TO_IGNORE):
     all_results = {}
     for method in ['mpis', 'global_is'] + iterative_methods:
         all_results[method] = load_results(model_name, method, False)
@@ -218,6 +234,8 @@ def plot_iterative_vs_IS_single_K_one_model(model_name, iterative_methods = ['vi
         colour = 'C' + str(i + 2)
 
         for lr_idx, lr in enumerate(all_results[method]['lrs']):
+            if lr in model_method_lrs_to_ignore[model_name].get(method, []):
+                continue
             alpha = DEFAULT_ALPHA_FUNC(lr_idx, len(all_results[method]['lrs']))
 
             ax[0].plot(all_results[method]['times']['elbos'][lr_idx].mean(-1).cumsum(0)[:x_lim_iters], all_results[method]['elbos'][lr_idx].mean(-1)[:x_lim_iters], label=f"{method.upper()} lr={lr}", color=colour, alpha=alpha)
@@ -238,6 +256,9 @@ def plot_iterative_vs_IS_single_K_one_model(model_name, iterative_methods = ['vi
     ax[2].set_ylabel('Total Variance')
     ax[3].set_ylabel('MSE')
 
+    ax[2].set_yscale('log')
+    ax[3].set_yscale('log')
+
     ax[-1].legend()
 
     fig.tight_layout()
@@ -246,13 +267,13 @@ def plot_iterative_vs_IS_single_K_one_model(model_name, iterative_methods = ['vi
     if save_pdf:
         plt.savefig(f"plots/{model_name}_iterative_vs_IS_all_K{mpis_K}-{global_is_K}{'_log_x' if log_x else ''}.pdf")
 
-def plot_iterative_vs_IS_single_K_all_models(iterative_methods = ['vi', 'rws', 'HMC'], mpis_K = 10, global_is_K = 1000, save_pdf=False, x_lim_iters=10, log_x=False):
+def plot_iterative_vs_IS_single_K_all_models(iterative_methods = ['vi', 'rws', 'HMC'], mpis_K = 10, global_is_K = 1000, save_pdf=False, x_lim_iters=10, log_x=False, model_method_lrs_to_ignore=DEFAULT_MODEL_METHOD_LRS_TO_IGNORE):
     all_results = {model_name: {} for model_name in ALL_MODEL_NAMES}
 
     if isinstance(mpis_K, int):
-        mpis_K = {method: mpis_K for method in ['mpis', 'global_is']+iterative_methods}
+        mpis_K = {method: mpis_K for method in ALL_MODEL_NAMES}
     if isinstance(global_is_K, int):
-        global_is_K = {method: global_is_K for method in ['mpis', 'global_is']+iterative_methods}
+        global_is_K = {method: global_is_K for method in ALL_MODEL_NAMES}
 
     for model_name in ALL_MODEL_NAMES:
         for method in ['mpis', 'global_is'] + iterative_methods:
@@ -273,7 +294,7 @@ def plot_iterative_vs_IS_single_K_all_models(iterative_methods = ['vi', 'rws', '
     for i, model_name in enumerate(ALL_MODEL_NAMES):
         for j, method in enumerate(['mpis', 'global_is']):
             Ks = all_results[model_name][method]['Ks']
-            K_idx = Ks.index(str(mpis_K[method]) if method == 'mpis' else str(global_is_K[method]))
+            K_idx = Ks.index(str(mpis_K[model_name]) if method == 'mpis' else str(global_is_K[model_name]))
 
             ax[0, i].plot(all_results[model_name][method]['times']['elbos'].mean(-1)[K_idx], all_results[model_name][method]['elbos'].mean(1)[K_idx], label=f"{method.upper()} K={Ks[K_idx]}", marker='*')
             ax[1, i].plot(all_results[model_name][method]['times']['p_ll'].mean(-1)[K_idx], all_results[model_name][method]['p_lls'].mean(1)[K_idx], label=f"{method.upper()} K={Ks[K_idx]}", marker='*')
@@ -282,18 +303,21 @@ def plot_iterative_vs_IS_single_K_all_models(iterative_methods = ['vi', 'rws', '
             colour = 'C' + str(j + 2)
 
             if method == 'vi' and model_name == 'occupancy':
-                for lr_idx, lr in enumerate(all_results['movielens'][method]['lrs']):
-                    alpha = DEFAULT_ALPHA_FUNC(lr_idx, len(all_results['movielens'][method]['lrs']))
+                # for lr_idx, lr in enumerate(all_results['bus_breakdown'][method]['lrs']):
 
-                    ax[0,i].plot([], [], label=f"{method.upper()} lr={lr}", color=colour, alpha=alpha)
-                    ax[1,i].plot([], [], label=f"{method.upper()} lr={lr}", color=colour, alpha=alpha)
+                alpha = DEFAULT_ALPHA_FUNC(lr_idx, len(all_results['movielens'][method]['lrs']) - len(model_method_lrs_to_ignore['movielens'].get(method, [])))
+
+                ax[0,i].plot([], [], label=f"{method.upper()}", color=colour, alpha=alpha)
+                ax[1,i].plot([], [], label=f"{method.upper()}", color=colour, alpha=alpha)
                 continue
 
             for lr_idx, lr in enumerate(all_results[model_name][method]['lrs']):
-                alpha = DEFAULT_ALPHA_FUNC(lr_idx, len(all_results[model_name][method]['lrs']))
+                if lr in model_method_lrs_to_ignore[model_name].get(method, []):
+                    continue
+                alpha = DEFAULT_ALPHA_FUNC(lr_idx, len(all_results[model_name][method]['lrs']) - len(model_method_lrs_to_ignore[model_name].get(method, [])))
 
-                ax[0, i].plot(all_results[model_name][method]['times']['elbos'][lr_idx].mean(-1).cumsum(0)[:x_lim_iters], all_results[model_name][method]['elbos'][lr_idx].mean(-1)[:x_lim_iters], label=f"{method.upper()} lr={lr}", color=colour, alpha=alpha)
-                ax[1, i].plot(all_results[model_name][method]['times']['p_ll'][lr_idx].mean(-1).cumsum(0)[:x_lim_iters], all_results[model_name][method]['p_lls'][lr_idx].mean(-1)[:x_lim_iters], label=f"{method.upper()} lr={lr}", color=colour, alpha=alpha)
+                ax[0, i].plot(all_results[model_name][method]['times']['elbos'][lr_idx].mean(-1).cumsum(0)[:x_lim_iters], all_results[model_name][method]['elbos'][lr_idx].mean(-1)[:x_lim_iters], label=f"{method.upper()}", color=colour, alpha=alpha)
+                ax[1, i].plot(all_results[model_name][method]['times']['p_ll'][lr_idx].mean(-1).cumsum(0)[:x_lim_iters], all_results[model_name][method]['p_lls'][lr_idx].mean(-1)[:x_lim_iters], label=f"{method.upper()}", color=colour, alpha=alpha)
 
         ax[0, i].set_title(model_name.upper())
 
@@ -318,33 +342,33 @@ def plot_iterative_vs_IS_single_K_all_models(iterative_methods = ['vi', 'rws', '
 
 
 if __name__ == '__main__':
-    mpis_K     = {'bus_breakdown': 30,     'chimpanzees': 10,     'movielens': 30,     'occupancy': 10},
-    global_is_K = {'bus_breakdown': 100000,'chimpanzees': 100000, 'movielens': 100000, 'occupancy': 100000},
+    mpis_K      = {'bus_breakdown': 30,    'chimpanzees': 10,     'movielens': 30,     'occupancy': 10}
+    global_is_K = {'bus_breakdown': 10000,'chimpanzees': 10000, 'movielens': 10000, 'occupancy': 10000}
     
     for model_name in ALL_MODEL_NAMES:
         plot_IS_per_K_one_model(model_name)
-        # if model_name == 'occupancy':
-        #     # plot_iterative_vs_IS_all_K_one_method(model_name, iterative_methods=['rws'])
-        #     plot_iterative_vs_IS_single_K_one_model(model_name, iterative_methods=['rws'], mpis_K=mpis_K[model_name], global_is_K=global_is_K[model_name])
-        # else:
-        #     # plot_iterative_vs_IS_all_K_one_method(model_name, iterative_methods=['vi', 'rws'])
-        #     plot_iterative_vs_IS_single_K_one_model(model_name, iterative_methods=['vi', 'rws'], mpis_K=mpis_K[model_name], global_is_K=global_is_K[model_name])
+        if model_name == 'occupancy':
+            plot_iterative_vs_IS_all_K_one_model(model_name, iterative_methods=['rws'])
+            plot_iterative_vs_IS_single_K_one_model(model_name, iterative_methods=['rws'], mpis_K=mpis_K[model_name], global_is_K=global_is_K[model_name])
+        else:
+            plot_iterative_vs_IS_all_K_one_model(model_name, iterative_methods=['vi', 'rws'])
+            plot_iterative_vs_IS_single_K_one_model(model_name, iterative_methods=['vi', 'rws'], mpis_K=mpis_K[model_name], global_is_K=global_is_K[model_name])
     
     plots_IS_per_K_all_models()
-    # plot_iterative_vs_IS_single_K_all_models(iterative_methods=['vi', 'rws'],
-    #                                          mpis_K={'bus_breakdown': 30,
-    #                                                  'chimpanzees': 10,
-    #                                                  'movielens': 30,
-    #                                                  'occupancy': 10},
-    #                                          global_is_K = 100000)
+    plot_iterative_vs_IS_single_K_all_models(iterative_methods=['vi', 'rws'],
+                                             mpis_K={'bus_breakdown': 30,
+                                                     'chimpanzees': 10,
+                                                     'movielens': 30,
+                                                     'occupancy': 10},
+                                             global_is_K = 10000)
 
     plots_IS_per_K_all_models(x_axis_time=True)
-    # plot_iterative_vs_IS_single_K_all_models(iterative_methods=['vi', 'rws'],
-    #                                          mpis_K={'bus_breakdown': 30,
-    #                                                  'chimpanzees': 10,
-    #                                                  'movielens': 30,
-    #                                                  'occupancy': 10},
-    #                                          global_is_K = 100000,
-    #                                          x_lim_iters=1000,
-    #                                          log_x=True)
+    plot_iterative_vs_IS_single_K_all_models(iterative_methods=['vi', 'rws'],
+                                             mpis_K={'bus_breakdown': 30,
+                                                     'chimpanzees': 10,
+                                                     'movielens': 30,
+                                                     'occupancy': 10},
+                                             global_is_K = 10000,
+                                             x_lim_iters=1000,
+                                             log_x=True)
 
