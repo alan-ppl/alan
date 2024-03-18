ebaa032dc845b7ba7b9ad1050cdae149148cb427
ThomasHeap <thomaseheap@gmail.com>
2024-03-18 15:05:10 +0000

    changing the HMC runner

--- examples/runner_moments_HMC.py
+++ examples/runner_moments_HMC.py
@@ -85,11 +85,12 @@ def run_experiment(cfg):
         with open(job_status_file, "w") as f:
             f.write(f"Starting job.\n")
 
-    moments_collection = {}
+    moments_collection = []
 
     seed = 0
 
     for num_run in range(num_runs):
+        moments_collection.append({})
         num_failed = 0
         failed = True 
 
@@ -179,7 +180,7 @@ def run_experiment(cfg):
             MSEs[name] *= num_runs/(num_runs-1)
         
         
-        moments_collection[name] = moments_collection[name].mean(1)
+        moments_collection[num_run][name] = moments_collection[name].mean(1)
 
     to_pickle = {'p_lls': p_lls, 'MSEs': MSEs,
                 'times': times, 'num_runs': num_runs, 
